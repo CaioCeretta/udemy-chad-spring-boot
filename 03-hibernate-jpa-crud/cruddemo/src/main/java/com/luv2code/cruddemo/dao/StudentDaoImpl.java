d10d11e9487b7a84c64d21b4a5a3a8fb46514d95
CaioCeretta <caioceretta@gmail.com>
2025-08-19 14:18:54 -0300

    echo: Up to Saving a Java object with JPA lesson

--- 03-hibernate-jpa-crud/cruddemo/src/main/java/com/luv2code/cruddemo/dao/StudentDaoImpl.java
+++ 03-hibernate-jpa-crud/cruddemo/src/main/java/com/luv2code/cruddemo/dao/StudentDaoImpl.java
@@ -1,4 +1,28 @@
 package com.luv2code.cruddemo.dao;
 
-public class StudentDaoImpl {
+import com.luv2code.cruddemo.entity.Student;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+public class StudentDaoImpl implements StudentDAO{
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    public StudentDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+    // implement save method
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+
+
 }
