11f6de0c36fefab1dde48e68101b138464d111f4
CaioCeretta <caioceretta@gmail.com>
2025-09-01 00:10:42 -0300

    feat: Querying objects lesson

--- 03-hibernate-jpa-crud/cruddemo/src/main/java/com/luv2code/cruddemo/dao/StudentDaoImpl.java
+++ 03-hibernate-jpa-crud/cruddemo/src/main/java/com/luv2code/cruddemo/dao/StudentDaoImpl.java
@@ -3,12 +3,15 @@ package com.luv2code.cruddemo.dao;
 import com.luv2code.cruddemo.entity.Student;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class StudentDaoImpl implements StudentDAO{
+public class StudentDaoImpl implements StudentDAO {
 
     // define field for entity manager
     private EntityManager entityManager;
@@ -30,6 +33,28 @@ public class StudentDaoImpl implements StudentDAO{
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        // Create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("From Student ", Student.class);
+
+        // return query results
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery(
+                "From Student where lastName=:theData", Student.class);
+        // set query parameters
+
+        theQuery.setParameter("theData", theLastName);
+
+        // return query results
+        return theQuery.getResultList();
     }
 
 
